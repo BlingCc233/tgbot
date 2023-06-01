ac1e23e89ef989e5f960c392fea96a4a6ac59468
BlingCc <54300418+BlingCc233@users.noreply.github.com>
2023-06-02 00:22:52 +0800

    Add files via upload

---
+++ api.py
@@ -0,0 +1,217 @@
+__author__ = 'BlingCc'
+
+import glob
+import json
+import os
+import shutil
+
+import instaloader as instaloader
+import requests
+from flask import request
+from instaloader import Post
+
+TELEGRAM_API_TOKEN = 'your token'
+bottoken = TELEGRAM_API_TOKEN
+surl = r"https://api.telegram.org/bot" + bottoken + "/sendMessage"
+purl = r"https://api.telegram.org/bot" + bottoken + "/sendPhoto"
+vurl = r"https://api.telegram.org/bot" + bottoken + "/sendVideo"
+
+def cmds(message, chat_id, update):
+    if message == '/holidays':
+        print(message)
+        holidays(chat_id)
+    if message == "/hello":
+        print(message)
+        index(chat_id)
+    if message[0:6] == "/whats":
+        print(message)
+        send_message(chat_id, "🔐Clash订阅:\n")
+    if message == "/mihoyo":
+        print(message)
+        send_message(chat_id, os.popen("python3 /home/tgbot/mihoyo.py"))
+    if message == "/help":
+        print(message)
+        commands(chat_id)
+    if message == "/college":
+        print(message)
+        webs(chat_id, message)
+    if message == "/galgame":
+        print(message)
+        webs(chat_id, message)
+    if message[0:8] == "/insdown":
+        print(message)
+        insd(chat_id, message)
+
+
+
+
+
+
+def send_message(chat_id, message):
+    decodedata={}
+    decodedata["text"]=message
+    decodedata["chat_id"]=chat_id
+    response=requests.post(url=surl,data=decodedata)
+    print(response.text)
+
+def get_holidays():
+    #today = datetime.now().strftime('%Y-%m-%d')
+    url = f'https://date.nager.at/api/v3/NextPublicHolidaysWorldwide/'
+    try:
+        response = requests.get(url)
+        response.raise_for_status() # 如果响应状态码不为 200，则抛出异常
+        holidays = response.json()
+    except (requests.exceptions.RequestException, ValueError) as e:
+        raise Exception(f'Error occurred while fetching holidays: {str(e)}')
+
+    chinese_holidays = [holiday for holiday in holidays if holiday['countryCode'] == 'CN']
+    international_holidays = [holiday for holiday in holidays if holiday['global']]
+
+    return chinese_holidays, international_holidays
+
+def holidays(chat_id):
+    try:
+        chinese_holidays, international_holidays = get_holidays()
+        # 在这里编写处理假期数据的代码
+    except Exception as e:
+        print(f'Error occurred: {str(e)}')
+
+    message = 'Today\'s holidays:\n\n'
+
+    if chinese_holidays:
+        message += 'Chinese holidays:\n'
+        for holiday in chinese_holidays:
+            message += f"- {holiday['localName']} ({holiday['name']})\n"
+        message += '\n'
+
+    if international_holidays:
+        message += 'International holidays:\n'
+        for holiday in international_holidays:
+            message += f"- {holiday['name']} ({holiday['countryCode']})\n"
+
+    if not chinese_holidays and not international_holidays:
+        message += 'No holidays today.'
+
+
+    decodedata={}
+    decodedata["text"]=message
+    decodedata["chat_id"]=chat_id
+    response=requests.post(url=surl,data=decodedata)
+
+def index(chat_id):
+    # 定义自定义键盘
+    inline_keyboard = [[{'text': '👨🏼‍💻Developer', 'url': 'https://github.com/blingcc233'},
+                        {'text': '📖命令列表', 'callback_data': 'cmd'}],
+                       [{'text': '🔗源码', 'url': 'https://github.com/blingcc233/tgbot'},
+                        {'text': '⚙中文', 'url': 'https://t.me/setlanguage/zhcncc'}],
+                       [{'text': '👴の主页', 'url': 'https://blingcc.eu.org/'},
+                        {'text': '🧩反馈问题', 'url': 'https://t.me/BlingCc233'}]]
+
+    # 将自定义键盘转换为JSON格式字符串
+    reply_markup = {'inline_keyboard': inline_keyboard}
+    data = {
+        'chat_id': chat_id,
+        'text': 'Cc的机器人',
+        'reply_markup': json.dumps(reply_markup)
+    }
+    response = requests.post(surl, data=data)
+
+# 定义回调函数
+def button_callback(update, context):
+    query = update["callback_query"]
+    if query["data"] == 'cmd':
+        commands(query["from"]["id"])
+    elif query["data"] == 'world':
+        payload = {
+            'chat_id': query["from"]["id"],
+            'text': '您点击了World按钮！'
+        }
+        response = requests.post(surl, json=payload)
+
+def commands(chat_id):
+    theList = """
+        点击对应命令即可：
+        /hello -打个招呼
+        /holidays -holiday
+        /help -命令列表
+        /whats_the_subscribe -clash
+        /mihoyo -sign
+        /college -大学无忧
+        /galgame -gal资源
+        /insdown -ins下载
+    """
+    data = {
+        'chat_id': chat_id,
+        'text': theList
+    }
+    response = requests.post(surl, data=data)
+
+def webs(chat_id, message):
+    if message == "/galgame":
+        inline_keyboard = [[{'text': '🎮Gal', 'url': 'https://pan.zhacg.com/od2'}],
+                            [{'text': '(广告)MacApp', 'url': 'http://ahcos.com/list.php?fid=42'}]]
+    if message == "/college":
+        inline_keyboard = [[{'text': '📚网课资源', 'url': 'https://www.techfens.com/posts/buguake.html'}],
+                             [{'text': '(广告)WinApp', 'url': 'https://www.uy5.net/'}]]
+    reply_markup = {'inline_keyboard': inline_keyboard}
+    print(reply_markup)
+    data = {
+        'chat_id': chat_id,
+        'text': message[1:].upper(),
+        'reply_markup': json.dumps(reply_markup)
+    }
+    requests.post(surl, data=data)
+
+def insd(chat_id, message):
+    if message == "/insdown":
+        send_message(chat_id, "语法错误，请使用/insdown url下载图片")
+        return
+    url = message[9:]
+    index = url.find('/?')
+    if index != -1:
+        url = url[:index]
+
+    SHORTCODE = url.split('/')[-1]
+    # 使用Instaloader下载给定的URL的图片
+    L = instaloader.Instaloader()
+    post = Post.from_shortcode(L.context, SHORTCODE)
+    # 下载根据url截短出的shortcode的图片
+    L.download_post(post, target='insdown')
+    #打开文件夹
+    dir_path = 'insdown/'
+
+    # 获取子目录下所有jpg文件的文件路径
+    jpg_files = glob.glob(os.path.join(dir_path, '*.jpg'))
+    send_message(chat_id, "正在发送图片……(*^▽^*)")
+    # 打开所有jpg文件
+    for file_path in jpg_files:
+        with open(file_path, 'rb') as f:
+            # 发送图片
+            data = {
+                'chat_id': chat_id
+            }
+            files = {
+                'photo': f
+            }
+
+            requests.post(purl, data=data, files=files)
+            pass
+    # 获取子目录下所有mp4文件的文件路径
+    mp4_files = glob.glob(os.path.join(dir_path, '*.mp4'))
+    send_message(chat_id, "正在发送视频……(*^▽^*)")
+    # 打开所有mp4文件
+    for file_path in mp4_files:
+        with open(file_path, 'rb') as f:
+            # 发送视频
+            data = {
+                'chat_id': chat_id
+            }
+            files = {
+                'video': f
+            }
+            requests.post(vurl, data=data, files=files)
+            pass
+    # 删除文件夹
+    shutil.rmtree(dir_path)
+
+
---
+++ main.py
@@ -0,0 +1,72 @@
+__author__ = 'BlingCc'
+
+from flask import Flask, request
+import api
+import requests
+import time
+
+app = Flask(__name__)
+
+TELEGRAM_API_TOKEN = 'your token'
+bottoken = TELEGRAM_API_TOKEN
+rurl = r"https://api.telegram.org/bot" + bottoken + "/getUpdates"
+surl = r"https://api.telegram.org/bot" + bottoken + "/sendMessage"
+curl = r"https://api.telegram.org/bot" + bottoken + "/ansewrCallbackQuery"
+
+# def receive_message():
+#
+#     offset = None
+#     while True:
+#         try:
+#             response = requests.get(rurl, params={'offset': offset})
+#             print(response.text)
+#             data = response.json()
+#             for update in data['result']:
+#                 message = update["message"]
+#                 api.cmds(message["text"], message["chat"]["id"], update)
+#                 offset = update['update_id'] + 1
+#         except (requests.exceptions.RequestException, ValueError, KeyError) as e:
+#             print(f'Error occurred while fetching updates: {str(e)}')
+#         time.sleep(1)
+#     return 'OK'
+
+def get_updates(offset=None):
+    try:
+        params = {"offset": offset}
+        response = requests.get(rurl, params=params)
+        print(response.text)
+        if response.status_code != 200:
+            raise Exception("Failed to get updates")
+        data = response.json()
+        if data["ok"] and data["result"]:
+            for update in data["result"]:
+                # 如果是消息更新
+                if "message" in update:
+                    message = update["message"]
+                    # 处理消息
+                    api.cmds(message["text"], message["chat"]["id"], update)
+                # 如果是回调查询更新
+                elif "callback_query" in update:
+
+                    # 处理回调查询
+                    api.button_callback(update, None)
+
+                # 更新offset，以便获取下一批更新
+                offset = update["update_id"] + 1
+    except (requests.exceptions.RequestException, ValueError, KeyError) as e:
+        print(f'Error occurred while fetching updates: {str(e)}')
+    time.sleep(1)
+    # 继续获取更新
+    get_updates(offset)
+
+'''监听端口获取tg信息'''
+@app.route('/', methods=["POST"])
+def indexx():
+    return 'Hello, world!'
+
+
+
+
+if __name__ == '__main__':
+    get_updates()
+    app.run(debug=True)
